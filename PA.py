1f7e47caa35170ed6f94b732de6e64dc685d0b1f
Joshua Haley <joshua.haley@knights.ucf.edu>
2015-12-27 10:29:22 -0500

    Added argument parsing and file walk.

--- PA.py
+++ PA.py
@@ -6,12 +6,46 @@ it requires the exifread module which can be installed via pip
 """
 
 import exifread as ef
-import sys
-import getopt
+import os
+import argparse
 
-def main(argv):
-  print("Hello!  I'll organize your photos!!")
+verbose = False
+outputDir = "./out"
+inputDir ="."
+
+def main():
+    print("Hello!  I'll organize your photos!!")
+    parseArgs()
+
+    for root, dirs, filenames in os.walk(inputDir):
+        for f in filenames:
+            handleFile(f)
 
 
-if __name__ == "__main__": main(sys.argv)
+
+def parseArgs():
+    global verbose, outputDir, inputDir
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', dest='verbose', action='store_true')
+    parser.add_argument('input')
+    parser.add_argument('output')
+    args = parser.parse_args()
+
+    if args.verbose:
+        verbose = True
+    outputDir = args.output
+    inputDir = args.input
+    if(verbose):
+        print("InputDir =" + inputDir)
+        print("OutputDir =" + outputDir)
+
+
+def handleFile(file):
+    print("TODO: Do Somthing with" + file)
+
+if __name__ == "__main__":
+    main()
+
+
+
 
