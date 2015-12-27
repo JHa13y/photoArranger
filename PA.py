a7e3845484b95fcba6dc927adef2b4d296be8983
Joshua Haley <joshua.haley@knights.ucf.edu>
2015-12-27 09:44:27 -0500

    First Commit. Added Main.

---
+++ PA.py
@@ -0,0 +1,17 @@
+"""
+This program organizes photos/viedos recursively from an input directory
+to a Year/Month pattern in a specified output directory.
+
+it requires the exifread module which can be installed via pip
+"""
+
+import exifread as ef
+import sys
+import getopt
+
+def main(argv):
+  print("Hello!  I'll organize your photos!!")
+
+
+if __name__ == "__main__": main(sys.argv)
+
